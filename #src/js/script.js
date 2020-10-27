3e0633d82267da9cd2874aaec391ef2a1c5c2587
pavlovportfolio <dm.rom.pav@gmail.com>
2020-10-27 07:54:28 +0500

    mobile is ready

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,48 +1,159 @@
+//Получаем доступ ко всем элементам
+const
+	arrowl	= document.querySelector('.arrow_left'),
+	arrowr	= document.querySelector('.arrow_right'),
+	hor		= document.querySelector('.horizontal'),
+	allver	= document.querySelector('.allvertical'),
+	ver		= document.querySelectorAll('.vertical'),
+	horbut	= document.querySelectorAll('.horizontal__button'),
+	group	= document.querySelectorAll('.group');
 
+var
+	verbut	= [],
+	info	= [];
 
-var hslider = document.querySelector('.horizontal');
-var vslider = document.querySelectorAll('.vertical'); 
-var hbutton = document.querySelectorAll('.horizontal__button');
-var vbutton = [];
-for (let i = 0; i < vslider.length; i++) {
-	vbutton[i] = vslider[i].querySelectorAll('.vertical__button');
+for (let i = 0; i < ver.length; i++) {
+	verbut[i] 	= ver[i].querySelectorAll('.vertical__button');
 }
-var hindex = 0;
-var vindex = [0,0,0,0,0,0,0,0];
-
-for (let i = 0; i < hbutton.length; i++) {
-	hbutton[i].addEventListener("click", Hclick, false);
+for (let i = 0; i < group.length; i++) {
+	info[i] 	= group[i].querySelectorAll('.info');
 }
+
+
+
+// Индексирование все элементов по Х и У
+var
+	hind	= 0,
+	vind	= [0,0,0,0,0,0,0,0,0,0,0,0];
+
+
+
+// Размеры и расстояния
+var
+	vw				= window.innerWidth,
+	vh				= window.innerHeight,
+	horleftpos,
+	allverleftpos,
+	horlefttrans,
+	allverlefttrans,
+	allvertoppos,
+	vertoptrans;
+
+Resize();
+window.addEventListener('resize', Resize);
+function Resize() {
+	if (vw < 768) {
+		horleftpos 		= 0;
+		allverleftpos 	= 0;
+		horlefttrans 	= vw;
+		allverlefttrans	= 200;
+		allvertoppos	= 0;
+		vertoptrans		= 100;
+	}
+	else {
+		horleftpos		= 0.3 * vw,
+		allverleftpos	= 0.3 * vw,
+		horlefttrans	= 200,
+		allverlefttrans	= 200;
+		allvertoppos	= 0.3 * vh;
+		vertoptrans		= 100;
+	}
+	console.log('Resized:' + ' horleftpos:' + horleftpos + '; allverleftpos:' + allverleftpos + '; horlefttrans:' + horlefttrans + 
+	'; allverlefttrans:' + allverlefttrans + '; allvertoppos:' + allvertoppos + '; vertoptrans:' + vertoptrans);
+}
+
+
+
+// Действие
+for (let i = 0; i < horbut.length; i++) {
+	horbut[i].onclick = Hclick;
+}
+for (let i = 0; i < ver.length; i++) {
+	for (let j = 0; j < verbut[i].length; j++) {
+		verbut[i][j].onclick = Vclick;
+	}
+}
+arrowl.onclick = Aclick;
+arrowr.onclick = Aclick;
+
+// Задаём функции
 function Hclick() {
-	for (let i = 0; i < hbutton.length; i++) {
-		if (hbutton[i] == this) {
-			hindex = i;
-			console.log(hindex + ";" + vindex[hindex] + " - Horizontal click")
+	for (let i = 0; i < horbut.length; i++) {
+		if (this == horbut[i]) {
+			hind = i;
 		}
 	}
-	HclickMain();
+	Htrans();
+	console.log(hind + ':' + vind[hind] + " - Hclick");
 }
 
-function Vclick(event) {
-	for (let i = 0; i < vbutton[hindex].length; i++) {
-		if (vbutton[hindex][i] == this) {
-			if (i !== vindex[hindex]) {
-				event.preventDefault();
-				vindex[hindex] = i;
-				console.log(hindex + ";" + vindex[hindex] + " - use button");
-			}
-		}
-		else {
-			console.log(hindex + ";" + vindex[hindex] + " - use button");
+function Vclick() {
+	for (let i = 0; i < verbut[hind].length; i++) {
+		if (this == verbut[hind][i]) {
+			vind[hind] = i;
 		}
 	}
-	VclickMain();
+	Vtrans();
+	console.log(hind + ':' + vind[hind] + " - Vclick");
 }
 
-function HclickMain() {
-
+function Aclick() {
+	console.log (hind + ':' + vind[hind] + " - Aclick")
+	if (this == arrowl) {
+		hind--;
+		Htrans();
+	}
+	else if (this == arrowr) {
+		hind++;
+		Htrans();
+	}
 }
 
-function VclickMain() {
+function Htrans() {
+	//определим на сколько перемещать слайдеры
+	let
+		horx	= horleftpos - (hind * horlefttrans),
+		allverx	= allverleftpos - (hind * allverlefttrans);
+	// переместим
+	hor.style.left = horx + 'px';
+	allver.style.left = allverx + 'px';
+	// выделим выбранную кнопку
+	for (let i = 0; i < horbut.length; i++) {
+		horbut[i].classList.remove('horizontal__selected');
+		horbut[i].classList.add('horizontal__selectable');
+	}
+	horbut[hind].classList.remove('horizontal__selectable');
+	horbut[hind].classList.add('horizontal__selected');
+	// нужны ли стрелки?
+	if (hind == 0) {
+		arrowl.style.width = 0;
+	}
+	else if (hind == horbut.length - 1) {
+		arrowr.style.width = 0;
+	}
+	else {
+		arrowl.style.width = "20px";
+		arrowr.style.width = "20px";
+	}
+	// выберем другой вертикальный слайдер
+	for (let i = 0; i < ver.length; i++) {
+		ver[i].style.opacity	= 0;
+		ver[i].style.width		= 0;
+	}
+	ver[hind].style.opacity		= 1;
+	ver[hind].style.width 		= "auto";
+}
 
+function Vtrans() {
+	//определим на сколько перемещать слайдеры
+	let very =  - (vind[hind] * vertoptrans);
+	// переместим
+	ver[hind].style.top = very + 'px';
+	// выделим выбранную кнопку
+	for (let i = 0; i < verbut[hind].length; i++) {
+		verbut[hind][i].classList.remove('vertical__selected');
+		verbut[hind][i].classList.add('vertical__selectable');
+	}
+	verbut[hind][vind[hind]].classList.remove('vertical__selectable');
+	verbut[hind][vind[hind]].classList.add('vertical__selected');
 }
--- pavlov-mySite-project/js/script.js
+++ pavlov-mySite-project/js/script.js
@@ -1,48 +1,152 @@
 "use strict";
 
-var hslider = document.querySelector('.horizontal');
-var vslider = document.querySelectorAll('.vertical');
-var hbutton = document.querySelectorAll('.horizontal__button');
-var vbutton = [];
+//Получаем доступ ко всем элементам
+var arrowl = document.querySelector('.arrow_left'),
+    arrowr = document.querySelector('.arrow_right'),
+    hor = document.querySelector('.horizontal'),
+    allver = document.querySelector('.allvertical'),
+    ver = document.querySelectorAll('.vertical'),
+    horbut = document.querySelectorAll('.horizontal__button'),
+    group = document.querySelectorAll('.group');
+var verbut = [],
+    info = [];
 
-for (var i = 0; i < vslider.length; i++) {
-  vbutton[i] = vslider[i].querySelectorAll('.vertical__button');
+for (var i = 0; i < ver.length; i++) {
+  verbut[i] = ver[i].querySelectorAll('.vertical__button');
 }
 
-var hindex = 0;
-var vindex = [0, 0, 0, 0, 0, 0, 0, 0];
+for (var _i = 0; _i < group.length; _i++) {
+  info[_i] = group[_i].querySelectorAll('.info');
+} // Индексирование все элементов по Х и У
 
-for (var _i = 0; _i < hbutton.length; _i++) {
-  hbutton[_i].addEventListener("click", Hclick, false);
+
+var hind = 0,
+    vind = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // Размеры и расстояния
+
+var vw = window.innerWidth,
+    vh = window.innerHeight,
+    horleftpos,
+    allverleftpos,
+    horlefttrans,
+    allverlefttrans,
+    allvertoppos,
+    vertoptrans;
+Resize();
+window.addEventListener('resize', Resize);
+
+function Resize() {
+  if (vw < 768) {
+    horleftpos = 0;
+    allverleftpos = 0;
+    horlefttrans = vw;
+    allverlefttrans = 200;
+    allvertoppos = 0;
+    vertoptrans = 100;
+  } else {
+    horleftpos = 0.3 * vw, allverleftpos = 0.3 * vw, horlefttrans = 200, allverlefttrans = 200;
+    allvertoppos = 0.3 * vh;
+    vertoptrans = 100;
+  }
+
+  console.log('Resized:' + ' horleftpos:' + horleftpos + '; allverleftpos:' + allverleftpos + '; horlefttrans:' + horlefttrans + '; allverlefttrans:' + allverlefttrans + '; allvertoppos:' + allvertoppos + '; vertoptrans:' + vertoptrans);
+} // Действие
+
+
+for (var _i2 = 0; _i2 < horbut.length; _i2++) {
+  horbut[_i2].onclick = Hclick;
 }
+
+for (var _i3 = 0; _i3 < ver.length; _i3++) {
+  for (var j = 0; j < verbut[_i3].length; j++) {
+    verbut[_i3][j].onclick = Vclick;
+  }
+}
+
+arrowl.onclick = Aclick;
+arrowr.onclick = Aclick; // Задаём функции
 
 function Hclick() {
-  for (var _i2 = 0; _i2 < hbutton.length; _i2++) {
-    if (hbutton[_i2] == this) {
-      hindex = _i2;
-      console.log(hindex + ";" + vindex[hindex] + " - Horizontal click");
+  for (var _i4 = 0; _i4 < horbut.length; _i4++) {
+    if (this == horbut[_i4]) {
+      hind = _i4;
     }
   }
 
-  HclickMain();
+  Htrans();
+  console.log(hind + ':' + vind[hind] + " - Hclick");
 }
 
-function Vclick(event) {
-  for (var _i3 = 0; _i3 < vbutton[hindex].length; _i3++) {
-    if (vbutton[hindex][_i3] == this) {
-      if (_i3 !== vindex[hindex]) {
-        event.preventDefault();
-        vindex[hindex] = _i3;
-        console.log(hindex + ";" + vindex[hindex] + " - use button");
-      }
-    } else {
-      console.log(hindex + ";" + vindex[hindex] + " - use button");
+function Vclick() {
+  for (var _i5 = 0; _i5 < verbut[hind].length; _i5++) {
+    if (this == verbut[hind][_i5]) {
+      vind[hind] = _i5;
     }
   }
 
-  VclickMain();
+  Vtrans();
+  console.log(hind + ':' + vind[hind] + " - Vclick");
 }
 
-function HclickMain() {}
+function Aclick() {
+  console.log(hind + ':' + vind[hind] + " - Aclick");
 
-function VclickMain() {}
+  if (this == arrowl) {
+    hind--;
+    Htrans();
+  } else if (this == arrowr) {
+    hind++;
+    Htrans();
+  }
+}
+
+function Htrans() {
+  //определим на сколько перемещать слайдеры
+  var horx = horleftpos - hind * horlefttrans,
+      allverx = allverleftpos - hind * allverlefttrans; // переместим
+
+  hor.style.left = horx + 'px';
+  allver.style.left = allverx + 'px'; // выделим выбранную кнопку
+
+  for (var _i6 = 0; _i6 < horbut.length; _i6++) {
+    horbut[_i6].classList.remove('horizontal__selected');
+
+    horbut[_i6].classList.add('horizontal__selectable');
+  }
+
+  horbut[hind].classList.remove('horizontal__selectable');
+  horbut[hind].classList.add('horizontal__selected'); // нужны ли стрелки?
+
+  if (hind == 0) {
+    arrowl.style.width = 0;
+  } else if (hind == horbut.length - 1) {
+    arrowr.style.width = 0;
+  } else {
+    arrowl.style.width = "20px";
+    arrowr.style.width = "20px";
+  } // выберем другой вертикальный слайдер
+
+
+  for (var _i7 = 0; _i7 < ver.length; _i7++) {
+    ver[_i7].style.opacity = 0;
+    ver[_i7].style.width = 0;
+  }
+
+  ver[hind].style.opacity = 1;
+  ver[hind].style.width = "auto";
+}
+
+function Vtrans() {
+  //определим на сколько перемещать слайдеры
+  var very = -(vind[hind] * vertoptrans); // переместим
+
+  ver[hind].style.top = very + 'px'; // выделим выбранную кнопку
+
+  for (var _i8 = 0; _i8 < verbut[hind].length; _i8++) {
+    verbut[hind][_i8].classList.remove('vertical__selected');
+
+    verbut[hind][_i8].classList.add('vertical__selectable');
+  }
+
+  verbut[hind][vind[hind]].classList.remove('vertical__selectable');
+  verbut[hind][vind[hind]].classList.add('vertical__selected');
+}
