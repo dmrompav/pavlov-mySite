2aef9b8da104b52b9c7204bf6d22f14368ff8378
pavlovportfolio <dm.rom.pav@gmail.com>
2020-10-28 23:58:10 +0500

    no swipes, no texts

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,4 @@
-//Получаем доступ ко всем элементам
+// !Получаем доступ ко всем элементам =====================
 const
 	arrowl	= document.querySelector('.arrow_left'),
 	arrowr	= document.querySelector('.arrow_right'),
@@ -6,12 +6,12 @@ const
 	allver	= document.querySelector('.allvertical'),
 	ver		= document.querySelectorAll('.vertical'),
 	horbut	= document.querySelectorAll('.horizontal__button'),
-	group	= document.querySelectorAll('.group');
-
+	group	= document.querySelectorAll('.group'),
+	tapfield= document.querySelector('.tapfield'),
+	main	= document.querySelector('.main');
 var
 	verbut	= [],
 	info	= [];
-
 for (let i = 0; i < ver.length; i++) {
 	verbut[i] 	= ver[i].querySelectorAll('.vertical__button');
 }
@@ -21,14 +21,17 @@ for (let i = 0; i < group.length; i++) {
 
 
 
-// Индексирование все элементов по Х и У
+// !Индексирование все элементов по Х и У +  прочие элементы================
 var
 	hind	= 0,
 	vind	= [0,0,0,0,0,0,0,0,0,0,0,0];
+var
+	scroll	= true,
+	flip	= 0;
+	
 
 
-
-// Размеры и расстояния
+// !Размеры и расстояния =================================
 var
 	vw				= window.innerWidth,
 	vh				= window.innerHeight,
@@ -36,17 +39,21 @@ var
 	allverleftpos,
 	horlefttrans,
 	allverlefttrans,
+	hortoppos,
 	allvertoppos,
 	vertoptrans;
 
 Resize();
 window.addEventListener('resize', Resize);
 function Resize() {
+	vw	= window.innerWidth;
+	vh	= window.innerHeight;
 	if (vw < 768) {
 		horleftpos 		= 0;
 		allverleftpos 	= 0;
 		horlefttrans 	= vw;
 		allverlefttrans	= 200;
+		hortoppos		= 30;
 		allvertoppos	= 0;
 		vertoptrans		= 100;
 	}
@@ -55,16 +62,19 @@ function Resize() {
 		allverleftpos	= 0.3 * vw,
 		horlefttrans	= 200,
 		allverlefttrans	= 200;
+		hortoppos		= 0.3 * vh;
 		allvertoppos	= 0.3 * vh;
 		vertoptrans		= 100;
 	}
 	console.log('Resized:' + ' horleftpos:' + horleftpos + '; allverleftpos:' + allverleftpos + '; horlefttrans:' + horlefttrans + 
 	'; allverlefttrans:' + allverlefttrans + '; allvertoppos:' + allvertoppos + '; vertoptrans:' + vertoptrans);
+	Htrans();
+	Vtrans();
 }
 
 
 
-// Действие
+// !Действие ===========================================
 for (let i = 0; i < horbut.length; i++) {
 	horbut[i].onclick = Hclick;
 }
@@ -76,7 +86,10 @@ for (let i = 0; i < ver.length; i++) {
 arrowl.onclick = Aclick;
 arrowr.onclick = Aclick;
 
-// Задаём функции
+
+
+// !Задаём функции =====================================
+// *Клик по Horizontal buttons
 function Hclick() {
 	for (let i = 0; i < horbut.length; i++) {
 		if (this == horbut[i]) {
@@ -86,17 +99,44 @@ function Hclick() {
 	Htrans();
 	console.log(hind + ':' + vind[hind] + " - Hclick");
 }
-
-function Vclick() {
-	for (let i = 0; i < verbut[hind].length; i++) {
-		if (this == verbut[hind][i]) {
-			vind[hind] = i;
+// *Клик по Vertical buttons
+function Vclick(event) {
+	if (this !== verbut[hind][vind[hind]]) {
+		event.preventDefault();
+		for (let i = 0; i < verbut[hind].length; i++) {
+			if (this == verbut[hind][i]) {
+				vind[hind] = i;
+			}
+		}
+		Vtrans();
+		console.log(hind + ':' + vind[hind] + " - Vclick");
+	}
+	else {
+		if(!this.classList.contains('typenoclick') && !this.classList.contains('typelink')) {
+			if (this.classList.contains('typeflip')) {
+				let f = this.querySelectorAll('.flip');
+				if (flip == 0) {
+					flip = 1;
+					f[0].style.transform = "rotateY(180deg)";
+					f[1].style.transform = "rotateY(360deg)";
+				}
+				else {
+					flip = 0;
+					f[0].style.transform = "rotateY(0deg)";
+					f[1].style.transform = "rotateY(180deg)";
+				}
+				if (this.classList.contains('colortheme')) {
+					if (flip == 0) {document.body.style.background = "linear-gradient(#000 0%, #40407a 40%, #40407a 60%, #000 100%)";}
+					if (flip == 1) {document.body.style.background = "linear-gradient(#000 0%, #b33939 40%, #b33939 60%, #000 100%)";}
+				}
+			}
+			else {
+				CallInfo();
+			}
 		}
 	}
-	Vtrans();
-	console.log(hind + ':' + vind[hind] + " - Vclick");
 }
-
+// *Клик по Arrows
 function Aclick() {
 	console.log (hind + ':' + vind[hind] + " - Aclick")
 	if (this == arrowl) {
@@ -126,14 +166,14 @@ function Htrans() {
 	horbut[hind].classList.add('horizontal__selected');
 	// нужны ли стрелки?
 	if (hind == 0) {
-		arrowl.style.width = 0;
+		arrowl.style.width		= 0;
 	}
 	else if (hind == horbut.length - 1) {
-		arrowr.style.width = 0;
+		arrowr.style.width		= 0;
 	}
 	else {
-		arrowl.style.width = "20px";
-		arrowr.style.width = "20px";
+		arrowl.style.width		= "20px";
+		arrowr.style.width		= "20px";
 	}
 	// выберем другой вертикальный слайдер
 	for (let i = 0; i < ver.length; i++) {
@@ -143,10 +183,9 @@ function Htrans() {
 	ver[hind].style.opacity		= 1;
 	ver[hind].style.width 		= "auto";
 }
-
 function Vtrans() {
 	//определим на сколько перемещать слайдеры
-	let very =  - (vind[hind] * vertoptrans);
+	let very = 0 - (vind[hind] * vertoptrans);
 	// переместим
 	ver[hind].style.top = very + 'px';
 	// выделим выбранную кнопку
@@ -156,4 +195,93 @@ function Vtrans() {
 	}
 	verbut[hind][vind[hind]].classList.remove('vertical__selectable');
 	verbut[hind][vind[hind]].classList.add('vertical__selected');
+}
+function CallInfo() {
+	//Запретить скролл
+	scroll = false;
+	//Развернуть Info
+	group[hind].style.transform				= "scale(1)";
+	info[hind][vind[hind]].style.transform	= "scale(1)";
+	info[hind][vind[hind]].style.opacity	= "1";
+	main.style.zIndex						= "30";
+	tapfield.style.height					= "100vh";
+	//Скрыть Hor и Ver buttons
+	allver.style.left						= -vw + 'px';
+	hor.style.top							= -vh + 'px';
+	allver.style.opacity					= 0;
+	hor.style.opacity						= 0;
+	// Добавить крестик
+	let close = document.createElement('div');													//Вставить крестик в контент
+	close.className = "close";
+	close.innerHTML = "X";
+	info[hind][vind[hind]].prepend(close);
+	arrowl.style.width		= "0px";
+	arrowr.style.width		= "0px";
+	//Позволить свернуть info кликом сбоку
+	tapfield.onclick = CloseInfo;
+	//Позволить свернуть info кликом сбоку
+	document.querySelector('.close').onclick = CloseInfo;
+}
+
+function CloseInfo() {
+	console.log('close info');
+	scroll = true;
+	group[hind].style.transform				= "scale(0)";
+	info[hind][vind[hind]].style.transform	= "scale(0)";
+	info[hind][vind[hind]].style.opacity	= "0";
+	main.style.zIndex						= "0";
+	tapfield.style.height					= "0";
+	Vtrans();
+	Htrans();
+	hor.style.top							= hortoppos + 'px';
+	allver.style.opacity					= 1;
+	hor.style.opacity						= 1;
+	document.querySelector('.close').remove();
+	arrowl.style.width		= "20px";
+	arrowr.style.width		= "20px";
+	if (hind == 0) {
+		arrowl.style.width		= 0;
+	}
+	else if (hind == horbut.length - 1) {
+		arrowr.style.width		= 0;
+	}
+	else {
+		arrowl.style.width		= "20px";
+		arrowr.style.width		= "20px";
+	}
+}
+
+
+
+// *Скроллинг ======================================================
+const d = document;
+if (d.addEventListener) {
+	if ('onwheel' in document) {
+		d.addEventListener("wheel", onWheel);				// IE9+, FF17+, Ch31+
+	} else if ('onmousewheel' in document) {
+		d.addEventListener("mousewheel", onWheel);			// устаревший вариант события
+	} else {
+		d.addEventListener("MozMousePixelScroll", onWheel);	// Firefox < 17
+	}
+} else {
+	d.attachEvent("onmousewheel", onWheel);					// IE8-
+}
+function onWheel(e) {
+	if (scroll) {
+		let delta = e.deltaY || e.detail || e.wheelDelta;
+		if (verbut[hind].length > 1) {
+			if (vind[hind] < 1) {
+				if (delta > 0) { vind[hind]++ }
+			}
+			else if (vind[hind] < verbut[hind].length - 1) {
+				if (delta > 0) { vind[hind]++ }
+				else { vind[hind]-- }
+			}
+			else {
+				if (delta < 0) { vind[hind]-- }
+			}
+			Vtrans();
+			console.log(hind + ':' + vind[hind] + " - Scroll");
+		}
+	}
 }
--- pavlov-mySite-project/js/script.js
+++ pavlov-mySite-project/js/script.js
@@ -1,13 +1,15 @@
 "use strict";
 
-//Получаем доступ ко всем элементам
+// !Получаем доступ ко всем элементам =====================
 var arrowl = document.querySelector('.arrow_left'),
     arrowr = document.querySelector('.arrow_right'),
     hor = document.querySelector('.horizontal'),
     allver = document.querySelector('.allvertical'),
     ver = document.querySelectorAll('.vertical'),
     horbut = document.querySelectorAll('.horizontal__button'),
-    group = document.querySelectorAll('.group');
+    group = document.querySelectorAll('.group'),
+    tapfield = document.querySelector('.tapfield'),
+    main = document.querySelector('.main');
 var verbut = [],
     info = [];
 
@@ -17,11 +19,13 @@ for (var i = 0; i < ver.length; i++) {
 
 for (var _i = 0; _i < group.length; _i++) {
   info[_i] = group[_i].querySelectorAll('.info');
-} // Индексирование все элементов по Х и У
+} // !Индексирование все элементов по Х и У +  прочие элементы================
 
 
 var hind = 0,
-    vind = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; // Размеры и расстояния
+    vind = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var scroll = true,
+    flip = 0; // !Размеры и расстояния =================================
 
 var vw = window.innerWidth,
     vh = window.innerHeight,
@@ -29,27 +33,35 @@ var vw = window.innerWidth,
     allverleftpos,
     horlefttrans,
     allverlefttrans,
+    hortoppos,
     allvertoppos,
     vertoptrans;
 Resize();
 window.addEventListener('resize', Resize);
 
 function Resize() {
+  vw = window.innerWidth;
+  vh = window.innerHeight;
+
   if (vw < 768) {
     horleftpos = 0;
     allverleftpos = 0;
     horlefttrans = vw;
     allverlefttrans = 200;
+    hortoppos = 30;
     allvertoppos = 0;
     vertoptrans = 100;
   } else {
     horleftpos = 0.3 * vw, allverleftpos = 0.3 * vw, horlefttrans = 200, allverlefttrans = 200;
+    hortoppos = 0.3 * vh;
     allvertoppos = 0.3 * vh;
     vertoptrans = 100;
   }
 
   console.log('Resized:' + ' horleftpos:' + horleftpos + '; allverleftpos:' + allverleftpos + '; horlefttrans:' + horlefttrans + '; allverlefttrans:' + allverlefttrans + '; allvertoppos:' + allvertoppos + '; vertoptrans:' + vertoptrans);
-} // Действие
+  Htrans();
+  Vtrans();
+} // !Действие ===========================================
 
 
 for (var _i2 = 0; _i2 < horbut.length; _i2++) {
@@ -63,7 +75,8 @@ for (var _i3 = 0; _i3 < ver.length; _i3++) {
 }
 
 arrowl.onclick = Aclick;
-arrowr.onclick = Aclick; // Задаём функции
+arrowr.onclick = Aclick; // !Задаём функции =====================================
+// *Клик по Horizontal buttons
 
 function Hclick() {
   for (var _i4 = 0; _i4 < horbut.length; _i4++) {
@@ -74,18 +87,52 @@ function Hclick() {
 
   Htrans();
   console.log(hind + ':' + vind[hind] + " - Hclick");
-}
+} // *Клик по Vertical buttons
 
-function Vclick() {
-  for (var _i5 = 0; _i5 < verbut[hind].length; _i5++) {
-    if (this == verbut[hind][_i5]) {
-      vind[hind] = _i5;
+
+function Vclick(event) {
+  if (this !== verbut[hind][vind[hind]]) {
+    event.preventDefault();
+
+    for (var _i5 = 0; _i5 < verbut[hind].length; _i5++) {
+      if (this == verbut[hind][_i5]) {
+        vind[hind] = _i5;
+      }
+    }
+
+    Vtrans();
+    console.log(hind + ':' + vind[hind] + " - Vclick");
+  } else {
+    if (!this.classList.contains('typenoclick') && !this.classList.contains('typelink')) {
+      if (this.classList.contains('typeflip')) {
+        var f = this.querySelectorAll('.flip');
+
+        if (flip == 0) {
+          flip = 1;
+          f[0].style.transform = "rotateY(180deg)";
+          f[1].style.transform = "rotateY(360deg)";
+        } else {
+          flip = 0;
+          f[0].style.transform = "rotateY(0deg)";
+          f[1].style.transform = "rotateY(180deg)";
+        }
+
+        if (this.classList.contains('colortheme')) {
+          if (flip == 0) {
+            document.body.style.background = "linear-gradient(#000 0%, #40407a 40%, #40407a 60%, #000 100%)";
+          }
+
+          if (flip == 1) {
+            document.body.style.background = "linear-gradient(#000 0%, #b33939 40%, #b33939 60%, #000 100%)";
+          }
+        }
+      } else {
+        CallInfo();
+      }
     }
   }
+} // *Клик по Arrows
 
-  Vtrans();
-  console.log(hind + ':' + vind[hind] + " - Vclick");
-}
 
 function Aclick() {
   console.log(hind + ':' + vind[hind] + " - Aclick");
@@ -137,7 +184,7 @@ function Htrans() {
 
 function Vtrans() {
   //определим на сколько перемещать слайдеры
-  var very = -(vind[hind] * vertoptrans); // переместим
+  var very = 0 - vind[hind] * vertoptrans; // переместим
 
   ver[hind].style.top = very + 'px'; // выделим выбранную кнопку
 
@@ -149,4 +196,101 @@ function Vtrans() {
 
   verbut[hind][vind[hind]].classList.remove('vertical__selectable');
   verbut[hind][vind[hind]].classList.add('vertical__selected');
+}
+
+function CallInfo() {
+  //Запретить скролл
+  scroll = false; //Развернуть Info
+
+  group[hind].style.transform = "scale(1)";
+  info[hind][vind[hind]].style.transform = "scale(1)";
+  info[hind][vind[hind]].style.opacity = "1";
+  main.style.zIndex = "30";
+  tapfield.style.height = "100vh"; //Скрыть Hor и Ver buttons
+
+  allver.style.left = -vw + 'px';
+  hor.style.top = -vh + 'px';
+  allver.style.opacity = 0;
+  hor.style.opacity = 0; // Добавить крестик
+
+  var close = document.createElement('div'); //Вставить крестик в контент
+
+  close.className = "close";
+  close.innerHTML = "X";
+  info[hind][vind[hind]].prepend(close);
+  arrowl.style.width = "0px";
+  arrowr.style.width = "0px"; //Позволить свернуть info кликом сбоку
+
+  tapfield.onclick = CloseInfo; //Позволить свернуть info кликом сбоку
+
+  document.querySelector('.close').onclick = CloseInfo;
+}
+
+function CloseInfo() {
+  console.log('close info');
+  scroll = true;
+  group[hind].style.transform = "scale(0)";
+  info[hind][vind[hind]].style.transform = "scale(0)";
+  info[hind][vind[hind]].style.opacity = "0";
+  main.style.zIndex = "0";
+  tapfield.style.height = "0";
+  Vtrans();
+  Htrans();
+  hor.style.top = hortoppos + 'px';
+  allver.style.opacity = 1;
+  hor.style.opacity = 1;
+  document.querySelector('.close').remove();
+  arrowl.style.width = "20px";
+  arrowr.style.width = "20px";
+
+  if (hind == 0) {
+    arrowl.style.width = 0;
+  } else if (hind == horbut.length - 1) {
+    arrowr.style.width = 0;
+  } else {
+    arrowl.style.width = "20px";
+    arrowr.style.width = "20px";
+  }
+} // *Скроллинг ======================================================
+
+
+var d = document;
+
+if (d.addEventListener) {
+  if ('onwheel' in document) {
+    d.addEventListener("wheel", onWheel); // IE9+, FF17+, Ch31+
+  } else if ('onmousewheel' in document) {
+    d.addEventListener("mousewheel", onWheel); // устаревший вариант события
+  } else {
+    d.addEventListener("MozMousePixelScroll", onWheel); // Firefox < 17
+  }
+} else {
+  d.attachEvent("onmousewheel", onWheel); // IE8-
+}
+
+function onWheel(e) {
+  if (scroll) {
+    var delta = e.deltaY || e.detail || e.wheelDelta;
+
+    if (verbut[hind].length > 1) {
+      if (vind[hind] < 1) {
+        if (delta > 0) {
+          vind[hind]++;
+        }
+      } else if (vind[hind] < verbut[hind].length - 1) {
+        if (delta > 0) {
+          vind[hind]++;
+        } else {
+          vind[hind]--;
+        }
+      } else {
+        if (delta < 0) {
+          vind[hind]--;
+        }
+      }
+
+      Vtrans();
+      console.log(hind + ':' + vind[hind] + " - Scroll");
+    }
+  }
 }
